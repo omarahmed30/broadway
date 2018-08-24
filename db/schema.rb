ba9a98535358cec0b137a2d584dcef9a51d56e1c
omarahmed103 <omariahmed103@gmail.com>
2018-08-24 09:14:15 -0400

    Added navbar user_id column to play table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_035726) do
+ActiveRecord::Schema.define(version: 2018_08_24_131049) do
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_08_24_035726) do
     t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
