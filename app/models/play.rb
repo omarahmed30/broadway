bcd9a49b883a82b759f0fff3b48bbe6742b04bf5
omarahmed103 <omariahmed103@gmail.com>
2018-08-25 11:34:26 -0400

    Added Review model and created association between Review, Play, and Users

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,4 +1,8 @@
 class Play < ApplicationRecord
     belongs_to :user
     belongs_to :category
+    has_many :reviews
+
+    has_attached_file :play_img, styles: { play_index: "250x250>", play_show: "325x325>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_many :plays
+  has_many :reviews
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
